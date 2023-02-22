711ccfb4a2be05ee1fd55bd46cfcab4b7cc291c5
Dougy1203 <78436877+Dougy1203@users.noreply.github.com>
2023-02-22 10:37:15 -0700

    Update log_delete.py

--- BackEnd/log_delete/log_delete.py
+++ BackEnd/log_delete/log_delete.py
@@ -21,7 +21,7 @@ dynamo_client = boto3.resource(
     aws_secret_access_key= AWS_SECRET_ACCESS_KEY,
 )
 
-log_table = dynamo_client.Table("Logs")
+log_table = dynamo_client.Table("Log")
 
 mongo_client = MongoClient(MONGO_STRING)
 
@@ -33,17 +33,46 @@ users_collection = deficit_table["Users"]
 def lambda_handler(event, context):
     request_body = event['body']
     try:
-        print('my bed... right now')
-        print(request_body)
+        table_logs = log_table.query(
+            KeyConditionExpression=Key('email').eq(request_body['email'])
+        )
+        user = users_collection.find_one({'email' : request_body['email']})
+        logs = table_logs['Items']
+        if(logs):
+            for log in logs:
+                if(log['date'] == request_body['date']):
+                    logs.remove(log)
+                    log_table.update_item(
+                        Key={
+                            'email': request_body['email'],
+                        },
+                        UpdateExpression='SET #ts = :val1',
+                        ExpressionAttributeValues={
+                          ":val1": json.dumps(logs),
+                        },
+                        ExpressionAttributeNames={
+                          "#ts": "logs"
+                        }
+                    )
+                    print(f'Log Deleted With Date: {request_body["date"]}')
+                    return{
+                        'body' : f'Log Deleted With Date: {request_body["date"]}'
+                    }
+                else:
+                    return{
+                        'body' : 'Date Not Found:::: Try Again'
+                    }
+        else:
+            print(f'Log Not Found with Email: {request_body["email"]}')   
     except Exception as e:
         print(e)
 
 performer = lambda_handler({
     'body' : {
-        'email' : '',
+        'email' : 'liamd1203@gmail.com',
         'password' : '',
-        'log' : [],
-    }
+        'date' : '01/01/2020',
+        }
     },
     None
     )
